b8a5e0a47e7ed04d3c0aa4b61eb146193132df1b
Daniela Valadares <danielasvc.ti@gmail.com>
2025-10-20 05:40:09 -0300

    feat: add project to repository

---
+++ CloudKit/CloudKit/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  CloudKit
+//
+//  Created by Daniela Valadares on 20/10/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
